bd07e568a1746c964fdd8babbb2c7fa5b8e420d5
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-11-30 01:17:57 +0500

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const picsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original} >
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", `${picsMarkup}`);
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log(event.target);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: `alt`,
+    captionDelay: 250,
+  });
+});
